d7462368fd1ac1d1b6d9105ed67da0c359d1f484
parssa <parssak@gmail.com>
2020-11-04 13:55:56 -0500

    Added ScheduleTime

---
+++ phase1/src/Entities/ScheduleTime.java
@@ -0,0 +1,44 @@
+package Entities;
+
+/**
+ * This class is Immutable
+ *
+ * @author Parssa
+ */
+public class ScheduleTime {
+    private int day, month, year, hour, minute;
+
+
+    public ScheduleTime(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public ScheduleTime(int day, int month, int year, int hour, int minute) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+}
