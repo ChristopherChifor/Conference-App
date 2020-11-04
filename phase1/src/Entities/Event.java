7c13c95741cf5b92206680ee4a26a480b85055f6
parssa <parssak@gmail.com>
2020-11-04 14:25:14 -0500

    Added remove + add attendee to Event

--- phase1/src/Entities/Event.java
+++ phase1/src/Entities/Event.java
@@ -19,4 +19,19 @@ public class Event {
     public Event(String name) {
         this.name = name;
     }
+
+    public boolean removeAttendeeFromEvent(String username) {
+        // TODO discuss with TA
+        if (!attendees.containsKey(username)) {
+            return false;
+        }
+        attendees.remove(username);
+        return true;
+    }
+
+    public boolean addAttendeeToEvent(String username, User user) {
+        // TODO discuss with TA
+        attendees.put(username, user);
+        return true;
+    }
 }
