91faf9a436d7a7c8c774d045a2fec834a463e7ab
parssa <parssak@gmail.com>
2020-11-04 14:08:01 -0500

    Refactored SocialManager's methods

--- phase1/src/UseCases/SocialManager.java
+++ phase1/src/UseCases/SocialManager.java
@@ -13,27 +13,6 @@ public class SocialManager {
     ScheduleManager scheduleManager;
     AccountManager accountManager;
 
-    // key is the user's username, value is a list of usernames that are user's friend:
-    HashMap<String, List<String>> friendsDatabase;
-
-    // adds friend to users friend list
-    public boolean addFriend(String username, User friendUsername) {
-        // TODO
-        return false;
-    }
-
-    //removes friend from friend list
-    public boolean removeFriend(String username, String friendUsername) {
-        // TODO
-        return false;
-    }
-
-    // returns true if the two inputed usernames are friends
-    public boolean isFriend(String username, String otherUsername) {
-        // TODO
-        return false;
-    }
-
     // returns true if two usernames share an event in common
     public boolean inEventTogether(String username, String otherUsername) {
         // TODO
@@ -44,11 +23,5 @@ public class SocialManager {
     public boolean isAttendeeForSpeaker(String speaker, String attendee) {
         // TODO
         return false;
-    }
-
-    // returns a list of usernames of the friends of this user
-    public List<String> getFriends(String username) {
-        // TODO
-        return null;
     }
 }
