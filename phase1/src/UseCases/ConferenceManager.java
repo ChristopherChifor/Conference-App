4964ee12c09d52a4eb7b860faceca37db91655bc
chrischifor <christopher.chifor@mail.utoronto.ca>
2020-11-04 14:48:52 -0500

    Finished ConferenceManager

--- phase1/src/UseCases/ConferenceManager.java
+++ phase1/src/UseCases/ConferenceManager.java
@@ -2,6 +2,7 @@ package UseCases;
 
 import Entities.Event;
 import Entities.Schedule;
+import Entities.User;
 
 /**
  * @author Chris
@@ -10,23 +11,42 @@ public class ConferenceManager {
     ScheduleManager scheduleManager;
     AccountManager accountManager;
 
+    public ConferenceManager(ScheduleManager scheduleManager, AccountManager accountManager) {
+        this.scheduleManager = scheduleManager;
+        this.accountManager = accountManager;
+    }
+
+    /**
+     *
+     * @return true iff
+     *      1) attendee exists
+     *      2) event exists
+     *      3) event has not occurred
+     *      4) the event is not full
+     *
+     */
     public boolean canSignUpForEvent(String username, Event event){
         return !(accountManager.getUser(username) == null || !scheduleManager.eventExists(event) ||
-                scheduleManager.eventHasHappened(event) || scheduleManager.eventFull(event)) ;
+                scheduleManager.eventHasHappened(event) || scheduleManager.eventFull(event));
     }
 
+    /**
+     * @param event: the event the user will sign up for
+     * @param username: username of the user
+     * @return true if user is added to the event
+     */
     public boolean signUpForEvent(String username, Event event) {
-
-        if (canSignUpForEvent(username, event)){
-            // Todo
-            return true;
-        }
-        return false;
+        User thisUser = accountManager.getUser(username);
+        return event.addAttendeeToEvent(username, thisUser);
     }
 
+    /**
+     * @param event: the event the user cancells enrollment to
+     * @param username: username of the user
+     * @return true if user removed from event
+     */
     public boolean cancelEnrolment(String username, Event event) {
-        // TODO
-        return false;
+        return event.removeAttendeeFromEvent(username);
     }
 
     /**
@@ -35,8 +55,7 @@ public class ConferenceManager {
      * @return schedule of events this user is enrolled in
      */
     public Schedule enrolledEvents(String username) {
-        // TODO
-        return null;
+        return scheduleManager.getAttendeeEvents(username);
     }
 
 
