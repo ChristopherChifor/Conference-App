0f3eb382ce9f665356753838e197b204cb76db04
andrei <andrei.vilcan@gmail.com>
2020-11-05 16:50:01 -0500

    addToSchedule works

--- phase1/src/Entities/Schedule.java
+++ phase1/src/Entities/Schedule.java
@@ -10,8 +10,14 @@ public class Schedule {
     private HashMap<Time, HashMap<Room, Event>> schedule;
 
     public boolean addToSchedule(Room room, Event event, Time time) {
-        // TODO
-        return false;
+
+        if (schedule.get(time).containsKey(room)) return false;
+        else {
+            HashMap<Room, Event> innerMap = new HashMap<Room, Event>();
+            schedule.put(time, innerMap);
+            innerMap.put(room, event);
+            return true;
+        }
     }
 
     public HashMap<Time, HashMap<Room, Event>> getSchedule() {
