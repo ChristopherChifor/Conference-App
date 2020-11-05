7db02799272725be1fe22036aae7cc06b4ebf921
Seyed-Paya Hosseini-Jahromi <paya.hosseini@mail.utoronto.ca>
2020-11-04 21:34:17 -0500

    Added modifications to the class AccountManager

--- phase1/src/UseCases/AccountManager.java
+++ phase1/src/UseCases/AccountManager.java
@@ -10,37 +10,65 @@ import java.util.HashMap;
 public class AccountManager {
     private HashMap<String, User> users; // never share this
 
-    public boolean createUser(String username, String password, User.UserType type)
+    public boolean createUser(String name, String username, String password, User.UserType type)
     {
-        // TODO
-        //  make user
+
+        // If username has not been taken, return true, else return false.
+        // If username hasn't been taken, create a User class object with parameters name, username, password, and type.
+        // Check to see if username has been taken using the canCreateUser method
+        if (canCreateUser(username))
+        {
+            User newUser = new User(name, username, password, type);
+            // Add the user object to the users HashMap.
+            users.put(username, newUser);
+            return true;
+        }
         return false;
-    }
-    public boolean canCreateUser(String username) {
-        // TODO
-        //  verify if username taken
-        return false;
+
     }
 
-    public boolean changeUserType(String username, User.UserType _type)
+    public boolean canCreateUser(String username) {
+
+        // If the users HashMap contains a User value with the key, username, return false. If not, return true.
+        if (users.containsKey(username))
+        {
+            return false;
+        }
+        return true;
+
+    }
+
+    public boolean changeUserType(String username, User.UserType type)
     {
-        // TODO
-        //  delete if unused.
+
+        // If the users HashMap contains a User value with the key, username, set its UserType to type and return true.
+        // If not, do not do anything and return false.
+        if (users.containsKey(username))
+        {
+            users.get(username).setUserType(type);
+            return true;
+        }
         return false;
     }
 
     public String authenticateUser(String username, String password) {
-        // TODO
         //  if password is correct for username, return this username, else null.
+        if (users.get(username).getPassword().equals(password))
+        {
+            return username;
+        }
         return null;
     }
 
 
     User getUser(String username) {
-        // TODO
         //  returns the user with this username
+        //  returns null if username does not exist in the users HashMap
         //  ideally accessible only to other usecases
-        //  MESSAGE SLACK BEFORE IMPLENTING
+        if (users.containsKey(username))
+        {
+            return users.get(username);
+        }
         return null;
     }
 }
