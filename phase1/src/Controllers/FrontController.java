fa58cbb60b713caf4644960cc83533b00d645d9c
Payam Fakoorziba <payam.fakoorziba@mail.utoronto.ca>
2020-11-13 17:33:59 -0500

    FrontController first time done

--- phase1/src/Controllers/FrontController.java
+++ phase1/src/Controllers/FrontController.java
@@ -1,10 +1,16 @@
 package Controllers;
 
+import Entities.User;
 import Presenters.Presenter;
 import UseCases.AccountManager;
 
+import java.util.ArrayList;
 
-public class FrontController extends AbstractController{
+/**
+ * @author Payam
+ */
+
+public class FrontController extends AbstractController {
 
     private AccountManager accountManager;
 
@@ -14,21 +20,47 @@ public class FrontController extends AbstractController{
 
     @Override
     protected void executeCommand(String command) {
-
+        ArrayList<String> parsedCommand = parseCommand(command);
+        switch (parsedCommand.get(0)) {
+            case "/login":
+                if (!accountManager.userExists(parsedCommand.get(1))) {
+                    presenter.printLines("User with username " + parsedCommand.get(1) + " does not exist");
+                } else {
+                    if (accountManager.authenticateUser(parsedCommand.get(1), parsedCommand.get(2)) == null) {
+                        presenter.printLines("Username or password incorrect");
+                    } else {
+                        presenter.printLines("Log in successful");
+                        // TODO: send to MainController
+                    }
+                }
+            case "/signup":
+                String name = parsedCommand.get(1), username = parsedCommand.get(2),
+                        password = parsedCommand.get(3);
+                if (accountManager.canCreateUser(parsedCommand.get(2))) {
+                    accountManager.createUser(name, username, password, User.UserType.ATTENDEE);
+                } else {
+                    presenter.printLines("The username " + username + " already exists.");
+                }
+            case "/exit":
+                // TODO: Exit
+        }
     }
 
     @Override
     protected void parseInput(String input) {
-
+        // TODO: No parseInput money at the moment
     }
 
     @Override
     protected void startUp() {
-
+        String startUpMessage = "--- CONFERENCE APP --- \nType /help for options";
+        presenter.printLines(startUpMessage);
     }
 
     @Override
     protected void defineCommands() {
-
+        commands.put("/login", "Login as an existing user");
+        commands.put("/signup", "Sign up as a new user");
+        commands.put("/exit", "Exit the program");
     }
 }
