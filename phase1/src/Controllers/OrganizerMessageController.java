250c636cb0da400f66af94b9b7684a92d154ab2b
Seyed-Paya Hosseini-Jahromi <paya.hosseini@mail.utoronto.ca>
2020-11-13 16:14:43 -0500

    Added Javadoc to the class AccountManager and code to the OrganizerMessageController class

--- phase1/src/Controllers/OrganizerMessageController.java
+++ phase1/src/Controllers/OrganizerMessageController.java
@@ -3,10 +3,54 @@ package Controllers;
 import Presenters.Presenter;
 import UseCases.MessageManager;
 
-// TODO Paya
-//  Also please do the Javadocs on AccountManager
+import java.util.ArrayList;
+
+/**
+ * Controller for Handling messaging for organizers.
+ * @author Paya
+ */
 public class OrganizerMessageController extends MessageController{
     public OrganizerMessageController(MessageManager messageManager, String username, Presenter presenter) {
         super(messageManager, username, presenter);
     }
+
+    /**
+     *  Definitions of commands an organizer can do.
+     */
+    @Override
+    protected void defineCommands() {
+        commands.put("/open", "Opens a conversation.");
+        commands.put("/send", "Sends message to a person.");
+        commands.put("/sendToMany", "Sends a message to many people.");
+        commands.put("/inbox", "Displays inbox of all conversations");
+    }
+
+    /**
+     * Execute a command based on the String input
+     * @param input the input(String) containing information such as the command, the users, and the message body.
+     */
+    @Override
+    protected void executeCommand(String input) {
+        ArrayList<String> parsedCommand = parseCommand(input);
+        switch (parsedCommand.get(0)) {
+            case "/open":
+                if (parsedCommand.size() < 2) parseInput(input);
+                openConversation(parsedCommand.get(1));
+            case "/send":
+                if (parsedCommand.size() < 2) parseInput(input);
+                sendMessage(parsedCommand.get(1), parsedCommand.get(2));
+            case "/sendToMany":
+                if (parsedCommand.size() < 2) parseInput(input);
+                ArrayList<String> usersArrayList = new ArrayList(parsedCommand.subList(1,
+                        parsedCommand.size()-2));
+                sendMessageMany(usersArrayList, parsedCommand.get(2));
+            case "/inbox": {
+                getInbox();
+            }
+            default:
+                break;
+        }
+    }
+
+
 }
--- phase1/src/UseCases/AccountManager.java
+++ phase1/src/UseCases/AccountManager.java
@@ -6,21 +6,30 @@ import java.util.HashMap;
 import java.util.Set;
 
 /**
+ * The use case class for creating and authenticating users.
  * @author Paya
  */
 public class AccountManager {
     private HashMap<String, User> users; // never share this
 
+
+    /**
+     * Create a user with a name, username, password, and user type (Attendee, Organizer, Speaker) if the username
+     * hasn't been taken (Checked using the canCreateUser method.)
+     * @param name the name of the user(String) that is to be created.
+     * @param username the username of the user(String) that is to be created.
+     * @param password the password of the user(String) that is to be created.
+     * @param type the type of user(User.UserType) that is to be created.
+     * @return true if the user is created (when the username hasn't been taken).
+     * Else, return false.
+     */
     public boolean createUser(String name, String username, String password, User.UserType type)
     {
 
-        // If username has not been taken, return true, else return false.
-        // If username hasn't been taken, create a User class object with parameters name, username, password, and type.
-        // Check to see if username has been taken using the canCreateUser method
         if (canCreateUser(username))
         {
             User newUser = new User(name, username, password, type);
-            // Add the user object to the users HashMap.
+            // Add the User object to the users HashMap.
             users.put(username, newUser);
             return true;
         }
@@ -28,6 +37,13 @@ public class AccountManager {
 
     }
 
+
+    /**
+     * Specifies if a user can be created based on if the username has been taken or not.
+     * @param username the username of the user(String) that we want to check to see if we can create.
+     * @return true if the username hasn't been taken (which means a user with that username can be created.)
+     * Else, return false.
+     */
     public boolean canCreateUser(String username) {
 
         // If the users HashMap contains a User value with the key, username, return false. If not, return true.
@@ -35,9 +51,15 @@ public class AccountManager {
 
     }
 
+    /**
+     * Changes the type of the user.
+     * @param username the username of the user(String).
+     * @param type the type that we want the user's type to change to(User.UserType).
+     * @return true and change the user type if the username is valid (There exists a user with that username).
+     * Else, return false.
+     */
     public boolean changeUserType(String username, User.UserType type) {
         // If the users HashMap contains a User value with the key, username, set its UserType to type and return true.
-        // If not, do not do anything and return false.
         if (users.containsKey(username)) {
             users.get(username).setUserType(type);
             return true;
@@ -45,8 +67,14 @@ public class AccountManager {
         return false;
     }
 
+    /**
+     * Authenticate a user if their password matches with the password associated with their username.
+     * @param username the username of the user(String).
+     * @param password the password the user has entered(String).
+     * @return the username if the password entered matches with the password associated with the username.
+     * ELse, return null.
+     */
     public String authenticateUser(String username, String password) {
-        //  if password is correct for username, return this username, else null.
         if (users.get(username).getPassword().equals(password))
         {
             return username;
@@ -54,9 +82,13 @@ public class AccountManager {
         return null;
     }
 
-
+    /**
+     * Get a specific user based on their username.
+     * @param username the username of the user we are trying to get(String).
+     * @return that user if a user with that username exists.
+     * ELse, return null.
+     */
     User getUser(String username) {
-        //  returns the user with this username
         //  returns null if username does not exist in the users HashMap
         //  ideally accessible only to other usecases
         if (users.containsKey(username))
