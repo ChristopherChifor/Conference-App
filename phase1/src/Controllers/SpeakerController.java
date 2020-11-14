2610bcaa926b65a8b546b5d1e8a8b31dbb57805e
andrei <andrei.vilcan@gmail.com>
2020-11-14 13:15:04 -0500

    SpeakerController almost works

--- phase1/src/Controllers/SpeakerController.java
+++ phase1/src/Controllers/SpeakerController.java
@@ -1,17 +1,36 @@
 package Controllers;
 
+import Entities.Event;
 import Presenters.Presenter;
+import Entities.ScheduleTime;
+import UseCases.ScheduleManager;
+import UseCases.ConferenceManager;
 
-// TODO Andrei
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Andrei
+ */
 public class SpeakerController extends AbstractController {
 
-    protected SpeakerController(Presenter presenter) {
+    private ScheduleManager scheduleManager;
+    private final String username;
+
+    protected SpeakerController(ScheduleManager scheduleManager, String username, Presenter presenter) {
         super(presenter);
+        this.scheduleManager = scheduleManager;
+        this.username = username;
     }
 
     @Override
     protected void executeCommand(String command) {
-
+        ArrayList<String> parsedCommand = parseCommand(command);
+        switch (parsedCommand.get(0)) {
+            case"/viewMyTalks":
+                viewMyTalks();
+        }
     }
 
     @Override
@@ -21,11 +40,40 @@ public class SpeakerController extends AbstractController {
 
     @Override
     protected void startUp() {
-
+        String startUpMessage = "--- Speaker Account Menu --- \n Hello " + username + ". \n Type help for options";
+        presenter.printLines(startUpMessage);
     }
 
     @Override
     protected void defineCommands() {
+        commands.put("/viewMyTalks", "View my talks.");
+    }
 
+    /**
+     * Prints the talks the speaker speaks at.
+     */
+    void viewMyTalks() {
+
+    }
+
+
+    /**
+     *  (copied from AttendeeController)
+     *  Prints a schedule as follows:
+     *      At: TIME
+     *      (for each event at time)
+     *      EVENT in room: ROOM
+     */
+    void printSchedule(HashMap<ScheduleTime, HashMap<String, String>> schedule) {
+        for (Map.Entry<ScheduleTime, HashMap<String, String>> entry : schedule.entrySet()) {
+            String dateTime ="At: " +entry.getKey().getDateTime();
+            presenter.printLines(dateTime);
+
+            for (Map.Entry<String, String> event : entry.getValue().entrySet()) {
+                String eventInfo = event.getValue() + " in room: " + event.getKey();
+                presenter.printLines(eventInfo);
+            }
+        }
+        presenter.printLines();
     }
 }
