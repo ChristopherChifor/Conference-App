9d88b9ab92b9e59fe84f132e6eabf66d5bb730ed
Payam Fakoorziba <payam.fakoorziba@mail.utoronto.ca>
2020-11-15 23:24:39 -0500

    Javadoc 1

--- phase1/src/UseCases/SocialManager.java
+++ phase1/src/UseCases/SocialManager.java
@@ -16,6 +16,7 @@ public class SocialManager implements Serializable {
         this.scheduleManager = scheduleManager;
     }
 
+    // TODO UNUSED
     // returns true if two usernames share an event in common
     public boolean isInEventTogether(String username, String otherUsername) {
 
@@ -24,10 +25,17 @@ public class SocialManager implements Serializable {
         return isUserInSchedule(otherUsername, attendeeSchedule);
     }
 
-    private boolean isUserInSchedule(String otherUsername, Schedule attendeeSchedule) {
+    /**
+     * Method for checking if a user is in a given schedule
+     *
+     * @param username Username of the user
+     * @param attendeeSchedule The schedule to be checked
+     * @return true iff user is in attendeeSchedule
+     */
+    private boolean isUserInSchedule(String username, Schedule attendeeSchedule) {
         for (ScheduleTime time: attendeeSchedule.getSchedule().keySet()) {
             for (String room: attendeeSchedule.getSchedule().get(time).keySet()) {
-                if(scheduleManager.getEvent(attendeeSchedule.getSchedule().get(time).get(room)).getAttendees().contains(otherUsername)) {
+                if(scheduleManager.getEvent(attendeeSchedule.getSchedule().get(time).get(room)).getAttendees().contains(username)) {
                     return true;
                 }
             }
@@ -36,6 +44,7 @@ public class SocialManager implements Serializable {
     }
 
     // returns true if attendee is attending speaker's event.
+    // TODO UNUSED
     public boolean isAttendeeForSpeaker(String speaker, String attendee) {
         Schedule speakerSchedule = scheduleManager.getSpeakerEvents(speaker);
         return isUserInSchedule(attendee, speakerSchedule);
