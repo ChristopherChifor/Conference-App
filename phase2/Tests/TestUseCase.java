09544f2d876a6bbcbf776689e28597e6370029e7
Payam Fakoorziba <payam.fakoorziba@mail.utoronto.ca>
2020-12-10 16:20:04 -0500

    Test

--- phase2/Tests/TestUseCase.java
+++ phase2/Tests/TestUseCase.java
@@ -41,5 +41,15 @@ public class TestUseCase {
         AccountManager accountManager = new AccountManager();
         accountManager.createUser("Jason", "Jason_baba", "Jason123", UserType.ATTENDEE);
         assertEquals(false, accountManager.getUser("jason_baba") == null);
+        assertEquals(true, accountManager.getUser("jason_2") == null);
+    }
+
+    @Test
+    public void changeUserType() {
+        AccountManager accountManager = new AccountManager();
+        accountManager.createUser("Jason", "Jason_baba", "Jason123", UserType.ATTENDEE);
+        accountManager.changeUserType("Jason_baba", UserType.SPEAKER);
+        assertEquals(false, accountManager.getUser("jason_baba").getUserType() == UserType.ATTENDEE);
+        assertEquals(true, accountManager.getUser("jason_baba").getUserType() == UserType.SPEAKER);
     }
 }
