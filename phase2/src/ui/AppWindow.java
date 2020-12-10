98355c99d775d1f1fa8197b688da0db14f30aae6
Aleksejs Kalinins <alex.kalinins@mail.utoronto.ca>
2020-12-10 17:44:07 -0500

    Tweaks to AppWindow

--- phase2/src/ui/AppWindow.java
+++ phase2/src/ui/AppWindow.java
@@ -4,7 +4,11 @@ import Presenters.MainPresenter;
 import ui.view.View;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicLookAndFeel;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.*;
+
+import com.bulenkov.darcula.*;
 
 /**
  * The window for displaying gui of the app.
@@ -13,31 +17,56 @@ public class AppWindow extends JDialog {
     private MainPresenter mainPresenter;
     private JPanel panel;
     private JButton backButton = new JButton("Back");
+    private JButton toggleDark = new JButton("\u263D");
+    private boolean light = true;
 
     private JPanel currentView = null;
 
     public AppWindow(MainPresenter mainPresenter) {
         this.mainPresenter = mainPresenter;
-        panel = new JPanel(new BorderLayout(20,20));
-        backButton.addActionListener(e->goBack());
+        panel = new JPanel(new BorderLayout(20, 20));
+        backButton.addActionListener(e -> goBack());
+        toggleDark.addActionListener(e ->toggleDark());
 
-        JPanel buttonBar = new JPanel(new BorderLayout(10,10));
+        JPanel buttonBar = new JPanel(new BorderLayout(10, 10));
         buttonBar.add(backButton, BorderLayout.WEST);
-        buttonBar.add(Box.createRigidArea(new Dimension(200,30)), BorderLayout.CENTER);
+        buttonBar.add(Box.createRigidArea(new Dimension(200, 30)), BorderLayout.CENTER);
+        buttonBar.add(toggleDark, BorderLayout.EAST);
 
         panel.add(buttonBar, BorderLayout.NORTH);
 
         add(panel);
         setTitle("Conference App");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setSize(new Dimension(600,400));
+        setSize(new Dimension(600, 400));
         setVisible(true);
 
     }
 
-    public void showView(View view){
-        if(!(view instanceof JPanel)) throw new IllegalArgumentException("View must be JPanel");
-        if(currentView != null){
+    /**
+     * Changes look and feel.
+     */
+    private void toggleDark() {
+        BasicLookAndFeel laf = new DarculaLaf();
+        try {
+            if(light) {
+                UIManager.setLookAndFeel(laf);
+            }else{
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            }
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        light = !light;
+
+        repaint();
+        revalidate();
+    }
+
+    public void showView(View view) {
+        if (!(view instanceof JPanel)) throw new IllegalArgumentException("View must be JPanel");
+        if (currentView != null) {
             panel.remove(currentView);
         }
 
@@ -54,7 +83,7 @@ public class AppWindow extends JDialog {
     /**
      * Triggered when user presses back button
      */
-    private void goBack(){
+    private void goBack() {
         mainPresenter.back();
     }
 }
