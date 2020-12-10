35c15e81d06f4080db30323e3fef586a2cefc3fa
Seyed-Paya Hosseini-Jahromi <paya.hosseini@mail.utoronto.ca>
2020-12-10 14:56:44 -0500

    Updated some test cases in TestEntity.java and added some new test cases to TestUseCase.java

--- phase2/Tests/TestUseCase.java
+++ phase2/Tests/TestUseCase.java
@@ -1,2 +1,38 @@
+import Entities.User;
+import UseCases.AccountManager;
+import Util.UserType;
+import org.junit.*;
+import java.util.HashMap;
+import UseCases.MessageManager;
+import static org.junit.Assert.*;
+
 public class TestUseCase {
+
+
+    //TODO Test Cases for the MessageManager Use Case Class
+
+    @Test(timeout=100)
+    public void testSendMessageNull() {
+        AccountManager accountManager = new AccountManager();
+        MessageManager messageManager = new MessageManager(accountManager);
+        User user = new User("David","CSC207","123", UserType.ATTENDEE);
+        User user2 = new User("Sam","United Kingdom","java program", UserType.SPEAKER);
+        assertEquals(false, messageManager.sendMessage(null, "CSC207", "Hello"));
+        assertEquals(false, messageManager.sendMessage(null, null, "Hello"));
+        assertEquals(false, messageManager.sendMessage(null, null, null));
+        assertEquals(false, messageManager.sendMessage("United Kingdom",null, null));
+        assertEquals(false, messageManager.sendMessage("United Kingdom",null, "Hi"));
+    }
+
+
+    @Test
+    public void testCanMessageNull() {
+        AccountManager accountManager = new AccountManager();
+        MessageManager messageManager = new MessageManager(accountManager);
+        User user = new User("David","CSC207","123", UserType.ATTENDEE);
+        User user2 = new User("Sam","United Kingdom","java program", UserType.SPEAKER);
+        assertEquals(false, messageManager.canMessage(null, "CSC207"));
+        assertEquals(false, messageManager.canMessage(null, null));
+        assertEquals(false, messageManager.canMessage("United Kingdom", null));
+    }
 }
