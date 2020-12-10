10ed538ec3949b71ddff22c2ef40b538937752e1
h-meng8 <60942572+h-meng8@users.noreply.github.com>
2020-12-10 00:48:54 -0500

    Added a few things in EventController and ScheduleManager

--- phase2/src/Controllers/EventController.java
+++ phase2/src/Controllers/EventController.java
@@ -20,6 +20,22 @@ public class EventController {
         pdfConverter = new PDFConverter();
     }
 
+    /**
+     * Cancels enrolment of current user from event.
+     * @param eventName name of event.
+     */
+    void cancelEnrolment(String eventName, String username) {
+            scheduleManager.removeFromEvent(username, eventName);
+    }
+
+    /**
+     *  Signs up attendee for event
+     * @param eventName name of the event
+     */
+    public void signUpEvent(String eventName, String username) {
+        scheduleManager.signUpForEvent(username, eventName);
+    }
+
     public void convertScheduleToPDF(String filepath, String username) {
         // TODO
         ArrayList<ScheduleEntry> userEvents = scheduleManager.getAttendeeEvents(username);
