f92143bedb0805aefb1f42aacd309468464b8578
Aleksejs Kalinins <alex.kalinins@mail.utoronto.ca>
2020-12-11 10:59:53 -0500

    UserSettings changes

--- phase2/src/Presenters/UserSettingsPresenter.java
+++ phase2/src/Presenters/UserSettingsPresenter.java
@@ -1,5 +1,7 @@
 package Presenters;
 
+import Controllers.AccountController;
+import Util.UserType;
 import ui.view.UserSettingsView;
 import ui.view.View;
 
@@ -7,6 +9,7 @@ public class UserSettingsPresenter implements Presenter{
     private boolean organizerMode;
     private String username;
     private MainPresenter mainPresenter;
+    private AccountController accountController;
 
     /**
      * Constructor
@@ -17,6 +20,52 @@ public class UserSettingsPresenter implements Presenter{
         this.organizerMode = organizerMode;
         this.username = username;
         this.mainPresenter=mainPresenter;
+        accountController = new AccountController();
+    }
+
+    /**
+     * Getter for the user's name (i.e., real name)
+     * @return user's real name
+     */
+    public String getUserRealName(){
+        //TODO DO THIS!
+        return "John Smith";
+    }
+
+    /**
+     * Getter for user's password
+     * @return users password
+     */
+    public String getPassword(){
+        //TODO DO THIS!!!
+        return "";
+    }
+
+    /**
+     * Getter for user Type
+     * @return users type
+     */
+    public UserType getUserType(){
+        //TODO DO THIS!!!
+        return UserType.ATTENDEE;
+    }
+
+    /**
+     * Takes user to the messages of this user
+     */
+    public void goMessages(){
+        MessagePresenter mp = new MessagePresenter(username, mainPresenter);
+        mainPresenter.addPresenter(mp);
+    }
+
+    public void goEvents(){
+        //UserEventsPresenter up = new UserEventsPresenter(username, mainPresenter);
+        //TODO DO THIS ONCE USEREVENTSPRESENTER IS FIGURED OUT!!!!!
+    }
+
+    public void saveChanges(String password, UserType type){
+        accountController.changeUserType(username, type);
+        accountController.changeUserPassword(username, password);
     }
 
     @Override
